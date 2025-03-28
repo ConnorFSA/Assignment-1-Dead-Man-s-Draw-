dade50bf2e1e3f4cafa720e6bfcf3cf790d6aac3
ConnorFSA <133937825+ConnorFSA@users.noreply.github.com>
2025-03-28 12:04:24 +1030

    Initial commit

---
+++ Assignment 1; Dead Man's Draw++.cpp
@@ -0,0 +1,12 @@
+﻿// Assignment 1; Dead Man's Draw++.cpp : Defines the entry point for the application.
+//
+
+#include "Assignment 1; Dead Man's Draw++.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ Assignment 1; Dead Man's Draw++.h
@@ -0,0 +1,8 @@
+﻿// Assignment 1; Dead Man's Draw++.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
