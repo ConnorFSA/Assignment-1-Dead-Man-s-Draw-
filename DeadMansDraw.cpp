d00106a497a8a4e78aee9b5f9ec2cc47ae2a4b59
ConnorFSA <133937825+ConnorFSA@users.noreply.github.com>
2025-03-31 15:57:22 +1030

    Rename files and update CMake configuration

--- DeadMansDraw.cpp
+++ DeadMansDraw.cpp
@@ -1,7 +1,7 @@
-﻿// Assignment 1; Dead Man's Draw++.cpp : Defines the entry point for the application.
+﻿// DeadMansDraw.cpp : Defines the entry point for the application.
 //
 
-#include "Assignment 1; Dead Man's Draw++.h"
+#include "DeadMansDraw.h"
 
 using namespace std;
 
@@ -25,8 +25,8 @@ Game::~Game() {
 // Application Entry Point
 int main()
 {
-	Game game = new Game(); // Create game instance
-	game.run() // Run game
+	Game* game = new Game(); // Create game instance
+	game->run(); // Run game
 	delete game; // Free memory once game is finished
 	return 0;
 }
--- DeadMansDraw.h
+++ DeadMansDraw.h
@@ -1,4 +1,4 @@
-﻿// Assignment 1; Dead Man's Draw++.h : Include file for standard system include files,
+﻿// DeadMansDraw.h : Include file for standard system include files,
 // or project specific include files.
 
 #pragma once
