f955a9b84ffb9789dfa93b12c31eeb81cbf1b1b2
ConnorFSA <133937825+ConnorFSA@users.noreply.github.com>
2025-04-20 17:10:09 +0930

    Add kraken cards ability

--- GameCards/Kraken.cpp
+++ GameCards/Kraken.cpp
@@ -1,5 +1,6 @@
 #include "CardTypes.h"  
-#include "Card.h"  
+#include "Card.h"
+#include "../Player.h"
 
 Kraken::Kraken(Game* game, int value) : Card(game, value) {
 	_cardType = CardType::Kraken;
@@ -13,5 +14,22 @@ std::string Kraken::toString() const {
 }
 
 void Kraken::isPlayed() {
+    Player* currentPlayer = _game->getCurrentPlayer();
+    std::vector<Card*>& cards = _game->getDeck()->getCards();
+    std::vector<Card*>& discardCards = currentPlayer->getPlayArea()->getDiscardPile()->getCards();
 
+    for (int i = 0; i < 3 && !cards.empty(); i++) {
+		Card* card = cards.back();
+        cards.pop_back();
+
+        // Add to current player's play area
+        currentPlayer->getPlayArea()->addCard(card);
+        std::cout << "The kraken has draw a " << card->toString() << " card." << std::endl;
+
+        // Call the isPlayed function for the chosen card is player hasnt busted
+        if (currentPlayer->hasBusted()) {
+            return;
+        }
+        card->isPlayed();
+    }
 }
